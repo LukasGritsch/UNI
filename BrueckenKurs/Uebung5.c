d24c8301ab5157292541f1d198420c5ea8a9a296
LukasGritsch <lukasgritsch@aon.at>
2022-09-13 22:44:21 +0200

    More c exercise

--- BrueckenKurs/Uebung5.c
+++ BrueckenKurs/Uebung5.c
@@ -1,6 +1,18 @@
 #include<stdio.h>
 
+//Funktion prototype
+int add(int num1,int num2);
+
+//Main funktion call
 void main()
 {
-printf("Hello World!");
+printf("Hello World!\n");
+int hSum; 
+hSum = add(5,88);
+ printf("If you add 5 to 88 you will get: %d", hSum);
+}
+
+//Funktion decleration
+int add(int num1,int num2){
+    return num1+num2;
 }
