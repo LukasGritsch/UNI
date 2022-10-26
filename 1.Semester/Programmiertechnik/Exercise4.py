ec72c3e326367ff6cea4b4c1546c91db98525b3c
LukasGritsch <lukasgritsch@aon.at>
2022-10-26 17:52:29 +0200

    Exercise 4 Programmiertechnik solved and worked on my Portfolio

---
+++ 1.Semester/Programmiertechnik/Exercise4.py
@@ -0,0 +1,25 @@
+
+
+def convertNumber(n, B):
+	result = ''
+	while n > 0:
+		result = result + str(n%B)  
+		n = n//B
+   
+	return result[::-1]
+
+def convertWithoutTurn(n,B):
+	result=''
+	temLsit = []
+	while n > 0:
+		temLsit.insert(0,str(n%B))
+		n = n//B
+	result = result.join(temLsit)
+	return result
+
+
+print(convertNumber(128, 2))
+print(convertNumber(24, 8))
+
+print(convertWithoutTurn(128, 2))
+print(convertWithoutTurn(24, 8))
